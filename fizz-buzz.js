9eac8b131a3ea131670f810f582b6bc4795b212e
Saliha Popal <salihapopal@gmail.com>
2024-03-08 12:22:50 +0000

    writing test

--- fizz-buzz.js
+++ fizz-buzz.js
@@ -1,15 +1,14 @@
 const fizzBuzz = (num) => {
 
-  // if the number is divisible by 3 and 5, return 'FizzBuzz'
-  if (num % 3 === 0 && num % 5 === 0) return 'FizzBuzz';
+  if(num % 3 === 0 && num % 5 === 0) 
+  return 'FizzBuzz';
 
-  // if the number is divisible by 3, return 'Fizz'
-  if (num % 3 === 0) return 'Fizz';
+  if (num % 3 === 0) 
+  return 'Fizz';
 
-  // if the number is divisible by 5, return 'Buzz'
-  if (num % 5 === 0) return 'Buzz';
+  if(num % 5 === 0) 
+  return 'Buzz';
 
-  // otherwise, return the number
   return num;
 }
 
--- fizz-buzz.test.js
+++ fizz-buzz.test.js
@@ -1,6 +1,31 @@
 const { fizzBuzz } = require('./fizz-buzz');
 
-// 1. write a describe to contain our tests
-// 2. add a first test. You could use one of the console.log tests in the other file as a starting point
-// 3. run the test with `npm test` to see if it passes
-// 4. add another test, and another, and another, until you've tested all the functionality of the fizzBuzz function
+describe("fizzBuzz", () => {
+  it('returns the number is divisible by 3 and 5, return FizzBuzz', () => {
+    const result = fizzBuzz(15);
+    expect(result).toEqual('FizzBuzz');
+  })
+})  
+
+  describe("Fizz", () => {
+  it('returns the number is divisible by 3, return Fizz', () => {
+    const result = fizzBuzz(3);
+    expect(result).toEqual('Fizz');
+  })
+})
+
+describe("Buzz", () => {
+  it('should return the number is divisible by 5, return Buzz', () => {
+    const result = fizzBuzz(5);
+    expect(result).toEqual('Buzz');
+  })
+})
+
+describe("num", () => {
+  it('otherwise, return the number', () => {
+    const num = (fizzBuzz(7));
+    expect(num).toEqual(num)
+  })
+})
+
+

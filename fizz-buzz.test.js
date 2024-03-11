2fb894236103968563bea89e2c7e1ec50c4557f5
Afsha10 <unityofbeing1@gmail.com>
2024-03-11 23:21:27 +0000

    Write tests for a function called "fizzBuzz"
    * Start in fizz-buzz.js. Do some manual testing by running some console.logs like the ones at the bottom of the file.
    
    * Switch to fizz-buzz.test.js and write tests for the fizzBuzz function.
    
    * Run `npm test` to run the tests. Jest will automatically find the tests in fizz-buzz.test.js and run them.

--- fizz-buzz.test.js
+++ fizz-buzz.test.js
@@ -1,4 +1,32 @@
-const { fizzBuzz } = require('./fizz-buzz');
+const { fizzBuzz } = require("./fizz-buzz");
+
+describe("give FizzBuzz for multiples of 3 and 5", () => {
+  it("returns the string fizz-buzz for multiples of 3 and 5", () => {
+    const result = fizzBuzz(15);
+    expect(result).toEqual("FizzBuzz");
+  });
+});
+
+describe("give Fizz for 3", () => {
+  it("return the string fizz for multiples of 3", () => {
+    const result = fizzBuzz(6);
+    expect(result).toEqual("Fizz");
+  });
+});
+
+describe("give Buzz for 5", () => {
+  it("return the string buzz for multiples of 5", () => {
+    const result = fizzBuzz(10);
+    expect(result).toEqual("Buzz");
+  });
+});
+
+describe("give the number when not divisible by 3 or 5", () => {
+  it("return the same number when not a multiple of 3 or 5", () => {
+    const result = fizzBuzz(2);
+    expect(result).toEqual(2);
+  });
+});
 
 // 1. write a describe to contain our tests
 // 2. add a first test. You could use one of the console.log tests in the other file as a starting point
